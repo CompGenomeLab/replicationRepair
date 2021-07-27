0da2fa719ba630117a051884ddc040384196eff9
azgarian <cemazgari@gmail.com>
2021-07-27 17:28:00 +0300

    ignore final directory and add peak (temporary) filter code

--- workflow/rules/make_windows.smk
+++ workflow/rules/make_windows.smk
@@ -1,18 +1,18 @@
 
 rule make_windows:
     input:
-        region="results/regions/R21061297-EdUrep-UV3-5hrls_combined_peaks_repdomains_org.narrowPeak",
+        region="results/regions/{sample}.broadPeak",
         genome="resources/ref_genomes/hg19/genome_hg19.bed",
     output:
-        final="results/regions/R21061297-EdUrep-UV3-5hrls_combined_peaks_repdomains_windows_101_10.bed",
+        final="results/regions/{sample}_windows_201_100.broadPeak",
     params:
-        intervalLen="10",
-        windowNum="101",
+        intervalLen="100",
+        windowNum="201",
         rev="",
     log:
-        "logs/make_windows.log",
+        "logs/{sample}_make_windows.log",
     benchmark:
-        "logs/make_windows.benchmark.txt",
+        "logs/{sample}_make_windows.benchmark.txt",
     conda:
         "../envs/bed2fasta.yaml"
     shell:  
--- workflow/scripts/makewindows.sh
+++ workflow/scripts/makewindows.sh
@@ -1,46 +1,51 @@
+set -e
 
 strands=("-" "+" ".")
 names=(minus plus nostrand)
 
 #
 
-    awk -v a="$4" -v b="$5" '{print $1"\t"int(($2+$3)/2-a/2-a*(b-1)/2)"\t"int(($2+$3)/2+a/2+a*(b-1)/2)"\t"$4"\t"".""\t"$6}' $1 > temp1.bed
+    awk -v a="$4" -v b="$5" '{print $1"\t"int(($2+$3)/2-a/2-a*(b-1)/2)"\t"int(($2+$3)/2+a/2+a*(b-1)/2)"\t"$4"\t"".""\t"$6}' $1 > $1temp1.bed
   
-    python3 workflow/scripts/ExactMatchingIntervals.py -i temp1.bed -g $2 -o temp2.bed
+    python3 workflow/scripts/ExactMatchingIntervals.py -i $1temp1.bed -g $2 -o $1temp2.bed
 
     for i in $(seq 0 2); do
 
-        awk -v strand="${strands[$i]}" '{if($6==strand){print}}' temp2.bed > temp2_${names[$i]}.bed 
+        awk -v strand="${strands[$i]}" '{if($6==strand){print}}' $1temp2.bed > $1temp2_${names[$i]}.bed 
 
-        bedtools intersect -a temp2_${names[$i]}.bed -b temp2_${names[$i]}.bed -wa -wb | cut -f 1-6 | uniq -c | grep "^      1" | cut -c 9- > temp_unique_${names[$i]}.bed
+        bedtools intersect -a $1temp2_${names[$i]}.bed -b $1temp2_${names[$i]}.bed -wa -wb | cut -f 1-6 | uniq -c | grep "^      1" | cut -c 9- > $1temp_unique_${names[$i]}.bed
 
         if [ "$6" = "reverse" ]; then # if reverse numbering of the windows needed
 
-            bedtools makewindows -b temp_unique_${names[$i]}.bed -n $5 -i srcwinnum -reverse > temp_windowed_${names[$i]}.bed 
+            bedtools makewindows -b $1temp_unique_${names[$i]}.bed -n $5 -i srcwinnum -reverse > $1temp_windowed_${names[$i]}.bed 
 
         else
             
-            bedtools makewindows -b temp_unique_${names[$i]}.bed -n $5 -i srcwinnum > temp_windowed_${names[$i]}.bed 
+            bedtools makewindows -b $1temp_unique_${names[$i]}.bed -n $5 -i srcwinnum > $1temp_windowed_${names[$i]}.bed 
 
         fi
 
-        awk -v strand="${strands[$i]}" '{print $0"\t"".""\t"strand}' temp_windowed_${names[$i]}.bed > temp_windows_${names[$i]}.bed
+        awk -v strand="${strands[$i]}" '{print $0"\t"".""\t"strand}' $1temp_windowed_${names[$i]}.bed > $1temp_windows_${names[$i]}.bed
 
     done
 	
-    cat temp_windows_${names[0]}.bed temp_windows_${names[1]}.bed temp_windows_${names[2]}.bed > $3
-     
+    cat $1temp_windows_${names[0]}.bed $1temp_windows_${names[1]}.bed $1temp_windows_${names[2]}.bed > $3
+
+    
+
+    [ -s $3 ] && echo "File not empty" || { echo "File empty"; rm $3; exit 1; }
+
     # Remove Unnecessary
 
-        rm temp1.bed
-        rm temp2.bed
+        rm $1temp1.bed
+        rm $1temp2.bed
 
         for name in ${names[@]}; do
         
-            rm temp2_${name}.bed
-            rm temp_unique_${name}.bed
-            rm temp_windowed_${name}.bed
-            rm temp_windows_${name}.bed
+            rm $1temp2_${name}.bed
+            rm $1temp_unique_${name}.bed
+            rm $1temp_windowed_${name}.bed
+            rm $1temp_windows_${name}.bed
 
         done
     #
