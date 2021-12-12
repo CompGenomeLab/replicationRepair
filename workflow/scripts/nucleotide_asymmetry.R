397eb48176eec4824d97827a2260f37fba538c18
Cem Azgari <cazgari@login.lxd>
2021-12-12 13:05:43 +0300

    reformatting of the figure and table

--- workflow/scripts/nucleotide_asymmetry.R
+++ workflow/scripts/nucleotide_asymmetry.R
@@ -4,8 +4,8 @@ library(argparser)
 library(stringr)
 library(ggpubr)
 library(ggplot2)
-library(dplyr)
 library(reshape2)
+library(dplyr)
 library(patchwork)
 
 ######## Arguments ##########
@@ -16,6 +16,8 @@ p <- add_argument(p, "--o2", help="output 2-mer")
 p <- add_argument(p, "--o3", help="output 3-mer")
 p <- add_argument(p, "--o4", help="output 4-mer")
 p <- add_argument(p, "--o5", help="output 5-mer")
+p <- add_argument(p, "--comb", help="output combined")
+p <- add_argument(p, "--table", help="table that contains all the values")
 
 # Parse the command line arguments
 argv <- parse_args(p)
@@ -54,54 +56,6 @@ counts_df$repdomains <- windows[,2]
 counts_df$strand <- windows[,4]
 counts_df$strand <- gsub("\\(.*", "", counts_df$strand)
 
-#counts_right <- filter(counts_df, windows > 20)
-#counts_right[[1]] <- NULL  
-#counts_right[[1365]] <- NULL 
-#counts_right_agg <- aggregate(.~repdomains+strand, counts_right, mean)
-#counts_right_agg <- data.frame(t(counts_right_agg))
-#counts_right_agg$seq <- rownames(counts_right_agg)
-#counts_right_agg <- counts_right_agg[c(-1,-2),]
-#colnames(counts_right_agg) <- c("ERD_-", "LRD_-", "ERD_+", "LRD_+", "seq")
-#counts_right_agg$direction <- "Right Replicating"
-#rownames(counts_right_agg)<-NULL
-
-#counts_left <- filter(counts_df, windows < -20)
-#counts_left[[1]] <- NULL  
-#counts_left[[1365]] <- NULL 
-#counts_left_agg <- aggregate(.~repdomains+strand, counts_left, mean)
-#counts_left_agg <- data.frame(t(counts_left_agg))
-#counts_left_agg$seq <- rownames(counts_left_agg)
-#counts_left_agg <- counts_left_agg[c(-1,-2),]
-#colnames(counts_left_agg) <- c("ERD_-", "LRD_-", "ERD_+", "LRD_+", "seq")
-#counts_left_agg$direction <- "Left Replicating"
-#rownames(counts_left_agg)<-NULL
-
-#counts_agg <- rbind(counts_right_agg, counts_left_agg)
-#counts_molten <- melt(counts_agg, id = c("seq","direction"))
-#tags <- data.frame(str_split_fixed(counts_molten$variable, "_", -1))
-#counts_molten$repdomains <- tags[,1]
-#counts_molten$strand <- tags[,2]
-
-#counts_molten$strand <- factor(
-#  counts_molten$strand, levels = c("+","-"))
-
-#counts_molten <- counts_molten[,c(5,6,2,1,4)]
-
-#mer1 <- filter(counts_molten, nchar(seq) == 1)
-#mer2 <- filter(counts_molten, nchar(seq) == 2)
-#mer3 <- filter(counts_molten, nchar(seq) == 3)
-#mer4 <- filter(counts_molten, nchar(seq) == 4)
-#mer5 <- filter(counts_molten, nchar(seq) == 5)
-
-#p <- ggplot(data=mer1, aes(x=seq, y=value, fill=strand)) +
-#  geom_bar(stat = "identity", size = 1.5, 
-#           position=position_dodge2(padding = 0.05)) +
-#  xlab("Sequences") + 
-#  ylab("Percentage (%)") +
-#  facet_grid(~direction~repdomains)
-
-#p + p_format
-
 counts_total <- filter(counts_df, windows < -20 | windows > 20)
 
 counts_total$direction[ counts_total$windows > 0 ] <- "Right Replicating"
@@ -116,7 +70,7 @@ counts_total_df <- rbind(counts_total_right, counts_total_left)
 counts_total_df <- data_summary(counts_total_df, varname="value",
                             groupnames=c("direction", "repdomains", "strand", "variable"))
 
-complementary_list <- unique(as.array( counts_total_df$variable ))
+complementary_list <- rev(unique(as.array( counts_total_df$variable )))
 
 for (nuc_id in 1:length(complementary_list)){
   comp_nuc <- ""
@@ -132,6 +86,7 @@ for (nuc_id in 1:length(complementary_list)){
     }
     comp_nuc <- paste0(base, comp_nuc)
   }
+  counts_total_df$complementary[ counts_total_df$variable == complementary_list[nuc_id] ] <- comp_nuc
   if (comp_nuc == complementary_list[nuc_id]) {
     next
   } else if (complementary_list[nuc_id] %in% unique(as.array( counts_total_df$variable ))){
@@ -139,29 +94,39 @@ for (nuc_id in 1:length(complementary_list)){
   }
 }
 
-counts_total_dcast <- dcast(counts_total_df, direction + repdomains + variable ~ strand, value.var = "value")
+counts_total_dcast <- dcast(counts_total_df, direction + repdomains + variable + complementary ~ strand, 
+  value.var = "value")
 
 counts_total_dcast$diff <- abs(counts_total_dcast$`+` - counts_total_dcast$`-`)
 
-counts_total_final <- merge(counts_total_df, counts_total_dcast, by=c("direction", "repdomains", "variable"))
+counts_total_final <- merge(counts_total_df, counts_total_dcast, by=c("direction", "repdomains", 
+  "variable", "complementary"))
 
+counts_total_final <- counts_total_final[, c(1,2,3,4,5,6,7,10)]
 
-mer1 <- filter(counts_total_final, nchar(as.character(variable)) == 1)
-mer2 <- filter(counts_total_final, nchar(as.character(variable)) == 2)
-mer3 <- filter(counts_total_final, nchar(as.character(variable)) == 3)
-mer4 <- filter(counts_total_final, nchar(as.character(variable)) == 4)
-mer5 <- filter(counts_total_final, nchar(as.character(variable)) == 5)
+colnames(counts_total_final) <- c("direction", "replication_domains", "sequence", "complementary", "strand", 
+  "percentage", "sd", "diff_strands")
+
+write.table(counts_total_final, argv$table, quote = F, row.names = F, sep=",")
+
+mer1 <- filter(counts_total_final, nchar(as.character(sequence)) == 1)
+mer2 <- filter(counts_total_final, nchar(as.character(sequence)) == 2)
+mer3 <- filter(counts_total_final, nchar(as.character(sequence)) == 3)
+mer4 <- filter(counts_total_final, nchar(as.character(sequence)) == 4)
+mer5 <- filter(counts_total_final, nchar(as.character(sequence)) == 5)
 
 #### 1-mer ####
 
-p.mer1 <- ggplot(data=mer1, aes(x=reorder(variable, diff), y=value, fill=strand)) +
+p.mer1 <- ggplot(data=mer1, aes(x=reorder(sequence, diff_strands), y=percentage, fill=strand)) +
   #geom_boxplot(outlier.shape = NA) +
   geom_bar(stat = "identity", size = 1.5,
            position=position_dodge2(padding = 0.05), width=.8)  +
-  geom_errorbar(aes(ymin=value-sd, ymax=value+sd), width=0.2, position=position_dodge(.8)) +
-  geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
-  facet_grid(~repdomains~direction, scales="free") +
+  geom_errorbar(aes(ymin=percentage-sd, ymax=percentage+sd), width=0.2, position=position_dodge(.8)) +
+  #geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
+  facet_grid(~replication_domains~direction, scales="free") +
   coord_flip() + 
+  scale_y_continuous(breaks = c(-30, -20, -10, 10, 20, 30), 
+                     labels = c("30", "20", "10", "10", "20", "30")) +  
   xlab("Sequences") + 
   ylab("Percentage (%)") +
   scale_fill_manual(values = c("+" = "#0571b0", 
@@ -175,14 +140,16 @@ ggsave(argv$o1, width = 22, height = 18, units = "cm")
 
 #### 2-mer ####
 
-p.mer2 <- ggplot(data=mer2, aes(x=reorder(variable, diff), y=value, fill=strand)) +
+p.mer2 <- ggplot(data=mer2, aes(x=reorder(sequence, diff_strands), y=percentage, fill=strand)) +
   #geom_boxplot(outlier.shape = NA) +
   geom_bar(stat = "identity", size = 1.5,
            position=position_dodge2(padding = 0.05), width=.8)  +
-  geom_errorbar(aes(ymin=value-sd, ymax=value+sd), width=0.2, position=position_dodge(.8)) +
-  geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
-  facet_grid(~repdomains~direction, scales="free") +
+  geom_errorbar(aes(ymin=percentage-sd, ymax=percentage+sd), width=0.2, position=position_dodge(.8)) +
+  #geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
+  facet_grid(~replication_domains~direction, scales="free") +
   coord_flip() + 
+  scale_y_continuous(breaks = c(-12, -8, -4, 4, 8, 12), 
+                     labels = c("12", "8", "4", "4", "8", "12")) + 
   xlab("Sequences") + 
   ylab("Percentage (%)") +
   scale_fill_manual(values = c("+" = "#0571b0", 
@@ -196,24 +163,26 @@ ggsave(argv$o2, width = 22, height = 18, units = "cm")
 
 #### 3-mer ####
 
-mer3_erd <- filter(mer3, direction == "Left Replicating", strand == "+", repdomains == "ERD")
-mer3_lrd <- filter(mer3, direction == "Left Replicating", strand == "+", repdomains == "LRD")
+mer3_erd <- filter(mer3, direction == "Left Replicating", strand == "+", replication_domains == "ERD")
+mer3_lrd <- filter(mer3, direction == "Left Replicating", strand == "+", replication_domains == "LRD")
 
-mer3_erd$quants <- ntile(mer3_erd$diff, 32)
-mer3_lrd$quants <- ntile(mer3_lrd$diff, 32)
+mer3_erd$quants <- ntile(mer3_erd$diff_strands, 32)
+mer3_lrd$quants <- ntile(mer3_lrd$diff_strands, 32)
 mer3_reps <- rbind(mer3_erd, mer3_lrd)
-mer3_reps <- mer3_reps[,c(2,3,10)]
+mer3_reps <- mer3_reps[,c(2,3,9)]
 
-mer3 <- merge(mer3, mer3_reps, by=c("repdomains", "variable") )
+mer3 <- merge(mer3, mer3_reps, by=c("replication_domains", "sequence") )
 
-p.mer3 <- ggplot(data=subset(mer3, quants>22), aes(x=reorder(variable, diff), y=value, fill=strand)) +
+p.mer3 <- ggplot(data=subset(mer3, quants>22), aes(x=reorder(sequence, diff_strands), y=percentage, fill=strand)) +
   #geom_boxplot(outlier.shape = NA) +
   geom_bar(stat = "identity", size = 1.5,
            position=position_dodge2(padding = 0.05), width=.8)  +
-  geom_errorbar(aes(ymin=value-sd, ymax=value+sd), width=0.2, position=position_dodge(.8)) +
-  geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
-  facet_grid(~repdomains~direction, scales="free") +
-  coord_flip() +
+  geom_errorbar(aes(ymin=percentage-sd, ymax=percentage+sd), width=0.2, position=position_dodge(.8)) +
+  #geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
+  facet_grid(~replication_domains~direction, scales="free") +
+  coord_flip() + 
+  scale_y_continuous(breaks = c(-5, -4, -3, -2, -1, 1, 2, 3, 4, 5), 
+                     labels = c("5", "4", "3", "2", "1", "1", "2", "3", "4", "5")) +
   xlab("Sequences") + 
   ylab("Percentage (%)") +
   scale_fill_manual(values = c("+" = "#0571b0", 
@@ -227,24 +196,26 @@ ggsave(argv$o3, width = 22, height = 18, units = "cm")
 
 #### 4-mer ####
 
-mer4_erd <- filter(mer4, direction == "Left Replicating", strand == "+", repdomains == "ERD")
-mer4_lrd <- filter(mer4, direction == "Left Replicating", strand == "+", repdomains == "LRD")
+mer4_erd <- filter(mer4, direction == "Left Replicating", strand == "+", replication_domains == "ERD")
+mer4_lrd <- filter(mer4, direction == "Left Replicating", strand == "+", replication_domains == "LRD")
 
-mer4_erd$quants <- ntile(mer4_erd$diff, 136)
-mer4_lrd$quants <- ntile(mer4_lrd$diff, 136)
+mer4_erd$quants <- ntile(mer4_erd$diff_strands, 136)
+mer4_lrd$quants <- ntile(mer4_lrd$diff_strands, 136)
 mer4_reps <- rbind(mer4_erd, mer4_lrd)
-mer4_reps <- mer4_reps[,c(2,3,10)]
+mer4_reps <- mer4_reps[,c(2,3,9)]
 
-mer4 <- merge(mer4, mer4_reps, by=c("repdomains", "variable") )
+mer4 <- merge(mer4, mer4_reps, by=c("replication_domains", "sequence") )
 
-p.mer4 <- ggplot(data=subset(mer4, quants>126), aes(x=reorder(variable, diff), y=value, fill=strand)) +
+p.mer4 <- ggplot(data=subset(mer4, quants>126), aes(x=reorder(sequence, diff_strands), y=percentage, fill=strand)) +
   #geom_boxplot(outlier.shape = NA) +
   geom_bar(stat = "identity", size = 1.5,
            position=position_dodge2(padding = 0.05), width=.8)  +
-  geom_errorbar(aes(ymin=value-sd, ymax=value+sd), width=0.2, position=position_dodge(.8)) +
-  geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
-  facet_grid(~repdomains~direction, scales="free") +
+  geom_errorbar(aes(ymin=percentage-sd, ymax=percentage+sd), width=0.2, position=position_dodge(.8)) +
+  #geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
+  facet_grid(~replication_domains~direction, scales="free") +
   coord_flip() + 
+  scale_y_continuous(breaks = c(-2, -1.5, -1, -0.5, .5, 1, 1.5, 2), 
+                     labels = c("2", "1.5", "1", "0.5", "0.5", "1", "1.5", "2")) + 
   xlab("Sequences") + 
   ylab("Percentage (%)") +
   scale_fill_manual(values = c("+" = "#0571b0", 
@@ -258,24 +229,26 @@ ggsave(argv$o4, width = 22, height = 18, units = "cm")
 
 #### 5-mer ####
 
-mer5_erd <- filter(mer5, direction == "Left Replicating", strand == "+", repdomains == "ERD")
-mer5_lrd <- filter(mer5, direction == "Left Replicating", strand == "+", repdomains == "LRD")
+mer5_erd <- filter(mer5, direction == "Left Replicating", strand == "+", replication_domains == "ERD")
+mer5_lrd <- filter(mer5, direction == "Left Replicating", strand == "+", replication_domains == "LRD")
 
-mer5_erd$quants <- ntile(mer5_erd$diff, 512)
-mer5_lrd$quants <- ntile(mer5_lrd$diff, 512)
+mer5_erd$quants <- ntile(mer5_erd$diff_strands, 512)
+mer5_lrd$quants <- ntile(mer5_lrd$diff_strands, 512)
 mer5_reps <- rbind(mer5_erd, mer5_lrd)
-mer5_reps <- mer5_reps[,c(2,3,10)]
+mer5_reps <- mer5_reps[,c(2,3,9)]
 
-mer5 <- merge(mer5, mer5_reps, by=c("repdomains", "variable") )
+mer5 <- merge(mer5, mer5_reps, by=c("replication_domains", "sequence") )
 
-p.mer5 <- ggplot(data=subset(mer5, quants>502), aes(x=reorder(variable, diff), y=value, fill=strand)) +
+p.mer5 <- ggplot(data=subset(mer5, quants>502), aes(x=reorder(sequence, diff_strands), y=percentage, fill=strand)) +
   #geom_boxplot(outlier.shape = NA) +
   geom_bar(stat = "identity", size = 1.5,
            position=position_dodge2(padding = 0.05), width=.8)  +
-  geom_errorbar(aes(ymin=value-sd, ymax=value+sd), width=0.2, position=position_dodge(.8)) +
-  geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
-  facet_grid(~repdomains~direction, scales="free") +
+  geom_errorbar(aes(ymin=percentage-sd, ymax=percentage+sd), width=0.2, position=position_dodge(.8)) +
+  #geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
+  facet_grid(~replication_domains~direction, scales="free") +
   coord_flip() + 
+  scale_y_continuous(breaks = c(-0.75, -0.50, -0.25, .25, .5, .75), 
+                     labels = c("0.75", "0.5", "0.25", "0.25", "0.5", "0.75")) + 
   xlab("Sequences") + 
   ylab("Percentage (%)") +
   scale_fill_manual(values = c("+" = "#0571b0", 
@@ -287,3 +260,23 @@ p.mer5 <- p.mer5 + p_format +
 
 ggsave(argv$o5, width = 22, height = 18, units = "cm")
 
+
+#### plot C #####
+# plot C will be a drawing
+# we are creating an empty text for that part
+p.C <- wrap_elements(grid::textGrob(''))
+
+#### Combining Plots with Patchwork ####
+
+layout <- "
+AABB
+AACC
+"
+
+p.mer3 + p.mer1 + p.C +
+  plot_annotation(tag_levels = 'A') +
+  plot_layout(design = layout, guides="collect") & 
+  theme(plot.tag = element_text(hjust = -0.2, size = 12, face="bold"),
+    legend.position = "bottom")
+
+ggsave(argv$comb, width = 22, height = 18, units = "cm") 
