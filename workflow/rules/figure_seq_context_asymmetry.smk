f98744bc0a4538d62028e8a552dfc88745c9ce4b
Cem Azgari <cazgari@login.lxd>
2022-01-20 21:40:28 +0300

    adding mono to pentanucleotide plots again

--- workflow/rules/figure_seq_context_asymmetry.smk
+++ workflow/rules/figure_seq_context_asymmetry.smk
@@ -4,6 +4,11 @@ rule figure_seq_context_asymmetry:
     output:
         comb="results/plots/figure4_seq_asymmetry.pdf",
         table="results/table/seq_asymmetry_exp_obs.csv",
+        o1="results/plots/mer1.pdf",
+        o2="results/plots/mer2.pdf",
+        o3="results/plots/mer3.pdf",
+        o4="results/plots/mer4.pdf",
+        o5="results/plots/mer5.pdf",
     log:
         "logs/figure_seq_context_asymmetry.log",
     benchmark:
@@ -15,5 +20,10 @@ rule figure_seq_context_asymmetry:
         Rscript workflow/scripts/nucleotide_asymmetry.R \
         -i {input} \
         --comb {output.comb} \
+        --o1 {output.o1} \
+        --o2 {output.o2} \
+        --o3 {output.o3} \
+        --o4 {output.o4} \
+        --o5 {output.o5} \
         --exp_obs {output.table} &> {log}
         """
--- workflow/scripts/nucleotide_asymmetry.R
+++ workflow/scripts/nucleotide_asymmetry.R
@@ -12,6 +12,11 @@ library(GGally)
 ######## Arguments ##########
 p <- arg_parser("producing the sequence context of (1 to 5)-mer on initiation zones")
 p <- add_argument(p, "-i", help="input")
+p <- add_argument(p, "--o1", help="output 1-mer")
+p <- add_argument(p, "--o2", help="output 2-mer")
+p <- add_argument(p, "--o3", help="output 3-mer")
+p <- add_argument(p, "--o4", help="output 4-mer")
+p <- add_argument(p, "--o5", help="output 5-mer")
 p <- add_argument(p, "--comb", help="output combined")
 p <- add_argument(p, "--exp_obs", help="table that contains expected and observed lagging/leading")
 
@@ -231,3 +236,164 @@ p1 <- p1 + p_format +
                color="red") 
 
 ggsave(argv$comb, width = 11, height = 9, units = "cm") 
+
+counts_total_final <- merge(counts_total_df, counts_total_dcast, by=c("direction", "repdomains", 
+                                                                      "variable", "complementary"))
+
+counts_total_final <- counts_total_final[, c(1,2,3,4,5,6,7,10)]
+
+colnames(counts_total_final) <- c("direction", "replication_domains", "sequence", "complementary", "strand", 
+                                  "percentage", "sd", "diff_strands")
+
+#write.table(counts_total_final, argv$table, quote = F, row.names = F, sep=",")
+
+mer1 <- filter(counts_total_final, nchar(as.character(sequence)) == 1)
+mer2 <- filter(counts_total_final, nchar(as.character(sequence)) == 2)
+mer3 <- filter(counts_total_final, nchar(as.character(sequence)) == 3)
+mer4 <- filter(counts_total_final, nchar(as.character(sequence)) == 4)
+mer5 <- filter(counts_total_final, nchar(as.character(sequence)) == 5)
+
+#### 1-mer ####
+
+p.mer1 <- ggplot(data=mer1, aes(x=reorder(sequence, diff_strands), y=percentage, fill=strand)) +
+  #geom_boxplot(outlier.shape = NA) +
+  geom_bar(stat = "identity", size = 1.5,
+           position=position_dodge2(padding = 0.05), width=.8)  +
+  geom_errorbar(aes(ymin=percentage-sd, ymax=percentage+sd), width=0.2, position=position_dodge(.8)) +
+  #geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
+  facet_grid(~replication_domains~direction, scales="free") +
+  coord_flip() + 
+  scale_y_continuous(breaks = c(-30, -20, -10, 10, 20, 30), 
+                     labels = c("30", "20", "10", "10", "20", "30")) +  
+  xlab("Sequences") + 
+  ylab("Percentage (%)") +
+  scale_fill_manual(values = c("+" = "#0571b0", 
+                               "-" = "#ca0020")) 
+
+# adding and overriding the default plot format
+p.mer1 <- p.mer1 + p_format + 
+  theme(panel.spacing.x = unit(-0.5, "lines"))
+
+ggsave(argv$o1, width = 22, height = 18, units = "cm")
+
+#### 2-mer ####
+
+p.mer2 <- ggplot(data=mer2, aes(x=reorder(sequence, diff_strands), y=percentage, fill=strand)) +
+  #geom_boxplot(outlier.shape = NA) +
+  geom_bar(stat = "identity", size = 1.5,
+           position=position_dodge2(padding = 0.05), width=.8)  +
+  geom_errorbar(aes(ymin=percentage-sd, ymax=percentage+sd), width=0.2, position=position_dodge(.8)) +
+  #geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
+  facet_grid(~replication_domains~direction, scales="free") +
+  coord_flip() + 
+  scale_y_continuous(breaks = c(-12, -8, -4, 4, 8, 12), 
+                     labels = c("12", "8", "4", "4", "8", "12")) + 
+  xlab("Sequences") + 
+  ylab("Percentage (%)") +
+  scale_fill_manual(values = c("+" = "#0571b0", 
+                               "-" = "#ca0020")) 
+
+# adding and overriding the default plot format
+p.mer2 <- p.mer2 + p_format + 
+  theme(panel.spacing.x = unit(-0.5, "lines"))
+
+ggsave(argv$o2, width = 22, height = 18, units = "cm")
+
+#### 3-mer ####
+
+mer3_erd <- filter(mer3, direction == "Left Replicating", strand == "+", replication_domains == "ERD")
+mer3_lrd <- filter(mer3, direction == "Left Replicating", strand == "+", replication_domains == "LRD")
+
+mer3_erd$quants <- ntile(mer3_erd$diff_strands, 32)
+mer3_lrd$quants <- ntile(mer3_lrd$diff_strands, 32)
+mer3_reps <- rbind(mer3_erd, mer3_lrd)
+mer3_reps <- mer3_reps[,c(2,3,9)]
+
+mer3 <- merge(mer3, mer3_reps, by=c("replication_domains", "sequence") )
+
+p.mer3 <- ggplot(data=subset(mer3, quants>22), aes(x=reorder(sequence, diff_strands), y=percentage, fill=strand)) +
+  #geom_boxplot(outlier.shape = NA) +
+  geom_bar(stat = "identity", size = 1.5,
+           position=position_dodge2(padding = 0.05), width=.8)  +
+  geom_errorbar(aes(ymin=percentage-sd, ymax=percentage+sd), width=0.2, position=position_dodge(.8)) +
+  #geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
+  facet_grid(~replication_domains~direction, scales="free") +
+  coord_flip() + 
+  scale_y_continuous(breaks = c(-5, -4, -3, -2, -1, 1, 2, 3, 4, 5), 
+                     labels = c("5", "4", "3", "2", "1", "1", "2", "3", "4", "5")) +
+  xlab("Sequences") + 
+  ylab("Percentage (%)") +
+  scale_fill_manual(values = c("+" = "#0571b0", 
+                               "-" = "#ca0020")) 
+
+# adding and overriding the default plot format
+p.mer3 <- p.mer3 + p_format + 
+  theme(panel.spacing.x = unit(-0.5, "lines"))
+
+ggsave(argv$o3, width = 22, height = 18, units = "cm")
+
+#### 4-mer ####
+
+mer4_erd <- filter(mer4, direction == "Left Replicating", strand == "+", replication_domains == "ERD")
+mer4_lrd <- filter(mer4, direction == "Left Replicating", strand == "+", replication_domains == "LRD")
+
+mer4_erd$quants <- ntile(mer4_erd$diff_strands, 136)
+mer4_lrd$quants <- ntile(mer4_lrd$diff_strands, 136)
+mer4_reps <- rbind(mer4_erd, mer4_lrd)
+mer4_reps <- mer4_reps[,c(2,3,9)]
+
+mer4 <- merge(mer4, mer4_reps, by=c("replication_domains", "sequence") )
+
+p.mer4 <- ggplot(data=subset(mer4, quants>126), aes(x=reorder(sequence, diff_strands), y=percentage, fill=strand)) +
+  #geom_boxplot(outlier.shape = NA) +
+  geom_bar(stat = "identity", size = 1.5,
+           position=position_dodge2(padding = 0.05), width=.8)  +
+  geom_errorbar(aes(ymin=percentage-sd, ymax=percentage+sd), width=0.2, position=position_dodge(.8)) +
+  #geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
+  facet_grid(~replication_domains~direction, scales="free") +
+  coord_flip() + 
+  scale_y_continuous(breaks = c(-2, -1.5, -1, -0.5, .5, 1, 1.5, 2), 
+                     labels = c("2", "1.5", "1", "0.5", "0.5", "1", "1.5", "2")) + 
+  xlab("Sequences") + 
+  ylab("Percentage (%)") +
+  scale_fill_manual(values = c("+" = "#0571b0", 
+                               "-" = "#ca0020")) 
+
+# adding and overriding the default plot format
+p.mer4 <- p.mer4 + p_format + 
+  theme(panel.spacing.x = unit(-0.5, "lines"))
+
+ggsave(argv$o4, width = 22, height = 18, units = "cm")
+
+#### 5-mer ####
+
+mer5_erd <- filter(mer5, direction == "Left Replicating", strand == "+", replication_domains == "ERD")
+mer5_lrd <- filter(mer5, direction == "Left Replicating", strand == "+", replication_domains == "LRD")
+
+mer5_erd$quants <- ntile(mer5_erd$diff_strands, 512)
+mer5_lrd$quants <- ntile(mer5_lrd$diff_strands, 512)
+mer5_reps <- rbind(mer5_erd, mer5_lrd)
+mer5_reps <- mer5_reps[,c(2,3,9)]
+
+mer5 <- merge(mer5, mer5_reps, by=c("replication_domains", "sequence") )
+
+p.mer5 <- ggplot(data=subset(mer5, quants>502), aes(x=reorder(sequence, diff_strands), y=percentage, fill=strand)) +
+  #geom_boxplot(outlier.shape = NA) +
+  geom_bar(stat = "identity", size = 1.5,
+           position=position_dodge2(padding = 0.05), width=.8)  +
+  geom_errorbar(aes(ymin=percentage-sd, ymax=percentage+sd), width=0.2, position=position_dodge(.8)) +
+  #geom_hline(yintercept = 0, color = "black", linetype = "dashed") +
+  facet_grid(~replication_domains~direction, scales="free") +
+  coord_flip() + 
+  scale_y_continuous(breaks = c(-0.75, -0.50, -0.25, .25, .5, .75), 
+                     labels = c("0.75", "0.5", "0.25", "0.25", "0.5", "0.75")) + 
+  xlab("Sequences") + 
+  ylab("Percentage (%)") +
+  scale_fill_manual(values = c("+" = "#0571b0", 
+                               "-" = "#ca0020")) 
+
+# adding and overriding the default plot format
+p.mer5 <- p.mer5 + p_format + 
+  theme(panel.spacing.x = unit(-0.5, "lines"))
+
+ggsave(argv$o5, width = 22, height = 18, units = "cm")
